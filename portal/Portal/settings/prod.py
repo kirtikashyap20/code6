25da065fa228e08732cdbcc228e9d39de2a723e9
Kirti Kashyap <44333628+kirtikashyap20@users.noreply.github.com>
2023-08-05 00:21:17 -0400

    Update prod.py

--- portal/Portal/settings/prod.py
+++ portal/Portal/settings/prod.py
@@ -9,16 +9,9 @@ DEBUG = True
 SECRET_KEY = os.environ['SECRET_KEY']
 #ALLOWED_HOSTS = ['trump-prod.herokuapp.com','trumpportal.herokuapp.com','127.0.0.1','trump-staging.herokuapp.com']
 #ALLOWED_HOSTS = ['52.60.131.109','*','172.31.3.61']
-
+ROOT_URLCONF = 'vercel_app.urls'
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
 ALLOWED_HOSTS = ['35.182.169.92','.vercel.app', '127.0.0.1']
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'trumptransport',
-        'USER': 'admin',
-        'PASSWORD': 'Reducekp$12%',
-        'HOST': 'database-1.cplnq5cqf0rv.ca-central-1.rds.amazonaws.com',
-        'PORT': '3306',
-    }
+DATABASES = {}
 }
 print('prod.py')
