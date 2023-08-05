ee184a8307fb35a517c2ebccfb05bfa1261a7ec2
Kirti Kashyap <44333628+kirtikashyap20@users.noreply.github.com>
2023-08-05 00:24:22 -0400

    Update prod.py

--- portal/Portal/settings/prod.py
+++ portal/Portal/settings/prod.py
@@ -7,10 +7,6 @@ from .common import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 SECRET_KEY = os.environ['SECRET_KEY']
-#ALLOWED_HOSTS = ['trump-prod.herokuapp.com','trumpportal.herokuapp.com','127.0.0.1','trump-staging.herokuapp.com']
-#ALLOWED_HOSTS = ['52.60.131.109','*','172.31.3.61']
-ROOT_URLCONF = 'vercel_app.urls'
-WSGI_APPLICATION = 'vercel_app.wsgi.app'
 ALLOWED_HOSTS = ['35.182.169.92','.vercel.app', '127.0.0.1']
 DATABASES = {}
 }
